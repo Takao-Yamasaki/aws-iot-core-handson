4c653a390c5b8d78fa841d9d2af6afb2d912905b
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2024-07-11 12:03:20 +0900

    add:dashboard

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -2,3 +2,32 @@
 resource "aws_cloudwatch_log_group" "handson" {
   name = "/h4b/iot"
 }
+
+# ダッシュボードの作成
+resource "aws_cloudwatch_dashboard" "handson" {
+  dashboard_body = jsonencode(
+    {
+      widgets = [
+        {
+          height     = 6
+          properties = {
+            query   = <<-EOT
+              SOURCE '/h4b/iot' | fields TIMESTAMP, HUMIDITY, TEMPERATURE
+              | stats avg(TEMPERATURE),avg(HUMIDITY) by bin(1m)
+              | sort TIMESTAMP desc
+            EOT
+            region  = local.region
+            stacked = true
+            title   = "/h4b/iot"
+            view    = "timeSeries"
+          }
+          type       = "log"
+          width      = 24
+          x          = 0
+          y          = 0
+        },
+      ]
+    }
+  )
+  dashboard_name = "h4b-iot-20240711"
+}
